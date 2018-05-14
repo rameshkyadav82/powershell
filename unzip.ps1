0f2fddae4dc5ccf052aa4b8fda977c1f61744cef
rameshkyadav82 <rameshkyadav82@gmail.com>
2018-05-14 14:25:02 +0800

    Create unzip.ps1

---
+++ unzip.ps1
@@ -0,0 +1,9 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip "E:\Unix_arc.zip" "E:\test_zip"
