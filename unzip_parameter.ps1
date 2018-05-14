dfcac69330df5f51fcef58874d5337bd1d579150
rameshkyadav82 <rameshkyadav82@gmail.com>
2018-05-14 14:36:59 +0800

    Create unzip_parameter.ps1

---
+++ unzip_parameter.ps1
@@ -0,0 +1,11 @@
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+
+Expand-ZIPFile –File “E:\Unix_arc.zip" "E:\test_zip\test"
